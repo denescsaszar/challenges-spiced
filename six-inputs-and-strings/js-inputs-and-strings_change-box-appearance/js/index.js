23db8dc22eca17800c96000160f3a81deb84b4ba
Denes <csaszar.denes@gmail.com>
2023-03-23 15:11:23 +0100

    work in progress

---
+++ six-inputs-and-strings/js-inputs-and-strings_change-box-appearance/js/index.js
@@ -0,0 +1,49 @@
+/*
+
+Change the appearance of the box according to the values of the sliders (color, border radius, rotation)
+
+– [ ] Add an event listener for each input element.
+– [ ] When the slider control of inputColor is moved, change the background color of the box. Hint: You need to work with the hsl() functional notation for colors.
+– [ ] When the slider control of inputRadius is moved, change the border radius of the box. If the control reaches the right end, make the box look like a circle.
+– [ ] When the slider control of inputRotation is moved, rotate the box. Hint: You may need to work with the CSS property "transform" and a value that contains "rotate" and "deg".
+
+Hint: You can work with the property "style", just like you worked with the property "classList".
+
+For further information check MDN:
+- about inputs of type="range": https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/range
+- about hsl(): https://developer.mozilla.org/en-US/docs/Web/CSS/color_value/hsl
+- about transform & rotate: https://developer.mozilla.org/en-US/docs/Web/CSS/transform
+- about CSS declarations with "style": https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/style
+
+*/
+
+const inputColor = document.querySelector("#inputColor");
+
+const inputRadius = document.querySelector("#inputRadius");
+
+const inputRotation = document.querySelector("#inputRotation");
+
+const box = document.querySelector("[data-js='box']");
+// example.addEventListener("input", (evt) => {
+//   console.log("VALUE: ", evt.target);
+
+inputColor.addEventListener("input", (evt) => {
+    const val = evt.target.value;
+    // box.style.backgroundColor = "hsl(" + val + "50% 50%)";
+    box.style.backgroundColor = `hsl(${val}, 50%, 50%)`;
+    console.log(evt.target.value);
+});
+
+inputRadius.addEventListener("input", (evt) => {
+
+    const val = Event.target.value;
+    box.style(borderRadius) = `${val} %`;
+    console.log(evt.target.value);
+
+inputRotation.addEventListener("input", (evt) => {
+
+    const val = Event.target.value;
+    box.style.transform = `rotate(${val}deg)`;
+    console.log(Event.target.value);
+ });
+
---
+++ six-inputs-and-strings/js-inputs-and-strings_input-to-upper-and-lowercase/js/index.js
@@ -0,0 +1,33 @@
+console.clear();
+
+/*
+
+Convert the input value to lowercase when clicking a second button
+
+– [ ] Add a second button to the index.html . It should have a down arrow (&darr;) 
+as text content.
+– [ ] Assign the second button to a variable.
+– [ ] Add an event listener to the second button and have the input value 
+converted to lowercase when it is clicked.
+
+Hint: Access the value by using `.value` on the input element
+
+*/
+
+const input = document.querySelector('[data-js="first-input"]');
+const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
+
+uppercaseButton.addEventListener("click", () => {
+  input.value = input.value.toUpperCase();
+});
+
+// https://sebhastian.com/javascript-create-button/
+
+let lowercaseButton = document.createElement("second-button");
+lowercaseButton.innerHTML = "&darr;";
+
+document.body.appendChild(lowercaseButton);
+
+lowercaseButton.addEventListener("click", () => {
+  input.value = input.value.toLowerCase();
+});
