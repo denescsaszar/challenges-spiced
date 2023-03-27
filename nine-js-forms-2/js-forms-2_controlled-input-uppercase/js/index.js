b5a3fe0df70a426a0b4a4edd8ff7d56c06e6f70a
Denes <csaszar.denes@gmail.com>
2023-03-27 09:38:55 +0200

    nince-js-forms2

--- nine-js-forms-2/js-forms-2_controlled-input-uppercase/js/index.js
+++ nine-js-forms-2/js-forms-2_controlled-input-uppercase/js/index.js
@@ -8,11 +8,14 @@ input.addEventListener("keyup", (event) => {
 
 /*
 
-When a user enters a letter, it shall immediately be changed into a capital one.
+When a user enters a letter, it shall immediately be 
+changed into a capital one.
 
 Please consider
 
 - the `input` event as well as
-- how to read and write the value of a form field. There's definitely a difference between the value of the event's target and the (after-event) value of your input field.
+- how to read and write the value of a form field. There's 
+definitely a difference between the value of the event's 
+target and the (after-event) value of your input field.
 
 */
