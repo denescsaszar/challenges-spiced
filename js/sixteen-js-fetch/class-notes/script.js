915f2487fbb101c38c4a9b7cd8acf004f07fbe49
Denes <csaszar.denes@gmail.com>
2023-04-03 16:44:01 +0200

    js-fetch

---
+++ js/sixteen-js-fetch/class-notes/script.js
@@ -0,0 +1,25 @@
+
+
+const cohort = {
+    name: 'Web 19',
+    students: [
+        {   name: 'John',   
+            age: 23,
+            location: 'California'
+        },
+        {   name: 'Jane',
+            age: 24,
+            location: 'New York'
+        },
+        {   name: 'Joe',
+            age: 25,
+            location: 'Texas'}
+    ]
+
+        }
+
+
+
+try {
+    const toJson = JDON.stringify(cohort, );
+}
