d7b8f1fa3825ed9466d8242968f990b07ef67d14
Denes <csaszar.denes@gmail.com>
2023-04-11 17:40:45 +0200

    react-basics

---
+++ react/one-react-basics/react-basics_hello-world/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return <h1>Hello world</h1>;
+}
+
+function HelloWorld() {
+  return <h1>Hello world</h1>;
+}
