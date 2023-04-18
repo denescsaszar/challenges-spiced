e3681b5cd2a3cc5d248c9eb0046b8eead7217fb5
Denes <csaszar.denes@gmail.com>
2023-04-18 16:22:55 +0200

    one done

--- react/react-with-local-storage/react-with-local-storage_dice/src/components/D6Button/index.js
+++ react/react-with-local-storage/react-with-local-storage_dice/src/components/D6Button/index.js
@@ -1,20 +1,22 @@
-import { useState } from "react";
 import { getD6Roll } from "../../utils";
 import D6 from "../D6";
 import "./D6Button.css";
+import { useState } from "react";
 
-export function D6Button() {
+export function D6Button({ onRoll, value }) {
   const [rolls, setRolls] = useState([]);
 
   const handleRoll = () => {
-    setRolls([{ value: getD6Roll(), time: Date.now() }, ...rolls]);
+    const rollValue = getD6Roll();
+    console.log("Rolling the dice");
+    setRolls([{ value: rollValue, time: Date.now() }, ...rolls]);
+    console.log("New roll value: ", rollValue);
+    onRoll(rollValue);
   };
 
-  const currentRollValue = rolls[0]?.value;
-
   return (
-    <button className="d6-button" type="button" onClick={handleRoll}>
-      <D6 value={currentRollValue} />
+    <button className="D6Button" onClick={handleRoll}>
+      <D6 value={value} />
     </button>
   );
 }
--- react/react-with-local-storage/react-with-local-storage_journal-app-with-local-storage/src/App.js
+++ react/react-with-local-storage/react-with-local-storage_journal-app-with-local-storage/src/App.js
@@ -5,6 +5,7 @@ import EntryForm from "./components/EntryForm";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { uid } from "uid";
+import useLocalStorageState from "use-local-storage-state";
 
 const initialEntries = [
   {
@@ -39,6 +40,8 @@ const initialEntries = [
 function App() {
   const [entries, setEntries] = useState(initialEntries);
   const [filter, setFilter] = useState("all"); // "all" or "favorites"
+
+    
 
   function handleAddEntry(newEntry) {
     const date = new Date().toLocaleDateString("en-us", {
