ad790f8e3f0ee3968cdc152407144c4432f7d409
Denes <csaszar.denes@gmail.com>
2023-04-24 14:10:26 +0200

    test-version-1

--- react/eight-react-component-testing/components/Input/Input.test.js
+++ react/eight-react-component-testing/components/Input/Input.test.js
@@ -3,5 +3,19 @@ import userEvent from "@testing-library/user-event";
 import Input from ".";
 
 test("renders a label and an input with the correct attributes", () => {});
+render(<Input label="Name" />);
+const label = screen.getByText(/name/i);
+const input = screen.getByRole("textbox");
+
+expect(label).toBeInTheDocument();
+expect(input).toBeInTheDocument();
+expect(input).toHaveAttribute("type", "text");
+expect(input).toHaveAttribute("name", "name");
+expect(input).toHaveAttribute("id", "name");
 
 test("calls callback on every user input", async () => {});
+render(<Input label="Name" />);
+const input = screen.getByRole("textbox");
+
+await userEvent.type(input, "John");
+await userEvent.type(input, "Jane");
