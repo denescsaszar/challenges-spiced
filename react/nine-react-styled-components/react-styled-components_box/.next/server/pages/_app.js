d87d7972d6e2b0e68fdce766d3ccd76d71f7f4d8
Denes <csaszar.denes@gmail.com>
2023-04-26 10:46:53 +0200

    init

---
+++ react/nine-react-styled-components/react-styled-components_box/.next/server/pages/_app.js
@@ -0,0 +1,79 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/_app";
+exports.ids = ["pages/_app"];
+exports.modules = {
+
+/***/ "./pages/_app.js":
+/*!***********************!*\
+  !*** ./pages/_app.js ***!
+  \***********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ App)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _styles__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../styles */ \"./styles.js\");\n/* harmony import */ var _components_BoxWithClassName_BoxWithClassName_css__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../components/BoxWithClassName/BoxWithClassName.css */ \"./components/BoxWithClassName/BoxWithClassName.css\");\n/* harmony import */ var _components_BoxWithClassName_BoxWithClassName_css__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_components_BoxWithClassName_BoxWithClassName_css__WEBPACK_IMPORTED_MODULE_2__);\n\n\n\nfunction App({ Component , pageProps  }) {\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.Fragment, {\n        children: [\n            /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(_styles__WEBPACK_IMPORTED_MODULE_1__.GlobalStyle, {}, void 0, false, {\n                fileName: \"/Users/macbookohnenamen/Desktop/spiced-bootcamp/challenges-spiced/react/nine-react-styled-components/react-styled-components_box/pages/_app.js\",\n                lineNumber: 7,\n                columnNumber: 7\n            }, this),\n            /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(Component, {\n                ...pageProps\n            }, void 0, false, {\n                fileName: \"/Users/macbookohnenamen/Desktop/spiced-bootcamp/challenges-spiced/react/nine-react-styled-components/react-styled-components_box/pages/_app.js\",\n                lineNumber: 8,\n                columnNumber: 7\n            }, this)\n        ]\n    }, void 0, true);\n}\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9fYXBwLmpzLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7OztBQUFBO0FBQXdDO0FBQ3FCO0FBRTlDLFNBQVNDLElBQUksRUFBRUMsVUFBUyxFQUFFQyxVQUFTLEVBQUUsRUFBRTtJQUNwRCxxQkFDRTs7MEJBQ0UsOERBQUNILGdEQUFXQTs7Ozs7MEJBQ1osOERBQUNFO2dCQUFXLEdBQUdDLFNBQVM7Ozs7Ozs7O0FBRzlCLENBQUMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9yZWFjdC1zdHlsZWQtY29tcG9uZW50c19ib3gvLi9wYWdlcy9fYXBwLmpzP2UwYWQiXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHsgR2xvYmFsU3R5bGUgfSBmcm9tIFwiLi4vc3R5bGVzXCI7XG5pbXBvcnQgXCIuLi9jb21wb25lbnRzL0JveFdpdGhDbGFzc05hbWUvQm94V2l0aENsYXNzTmFtZS5jc3NcIjtcblxuZXhwb3J0IGRlZmF1bHQgZnVuY3Rpb24gQXBwKHsgQ29tcG9uZW50LCBwYWdlUHJvcHMgfSkge1xuICByZXR1cm4gKFxuICAgIDw+XG4gICAgICA8R2xvYmFsU3R5bGUgLz5cbiAgICAgIDxDb21wb25lbnQgey4uLnBhZ2VQcm9wc30gLz5cbiAgICA8Lz5cbiAgKTtcbn1cbiJdLCJuYW1lcyI6WyJHbG9iYWxTdHlsZSIsIkFwcCIsIkNvbXBvbmVudCIsInBhZ2VQcm9wcyJdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./pages/_app.js\n");
+
+/***/ }),
+
+/***/ "./styles.js":
+/*!*******************!*\
+  !*** ./styles.js ***!
+  \*******************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"GlobalStyle\": () => (/* binding */ GlobalStyle)\n/* harmony export */ });\n/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! styled-components */ \"styled-components\");\n/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(styled_components__WEBPACK_IMPORTED_MODULE_0__);\n\nconst GlobalStyle = styled_components__WEBPACK_IMPORTED_MODULE_0__.createGlobalStyle`\n  *,\n  *::before,\n  *::after {\n    box-sizing: border-box;\n  }\n\n  body {\n    margin: 0;\n    font-family: system-ui;\n  }\n`;\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zdHlsZXMuanMuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQXNEO0FBRS9DLE1BQU1DLGNBQWNELGdFQUFpQixDQUFDOzs7Ozs7Ozs7OztBQVc3QyxDQUFDLENBQUMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9yZWFjdC1zdHlsZWQtY29tcG9uZW50c19ib3gvLi9zdHlsZXMuanM/NmE3MyJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgeyBjcmVhdGVHbG9iYWxTdHlsZSB9IGZyb20gXCJzdHlsZWQtY29tcG9uZW50c1wiO1xuXG5leHBvcnQgY29uc3QgR2xvYmFsU3R5bGUgPSBjcmVhdGVHbG9iYWxTdHlsZWBcbiAgKixcbiAgKjo6YmVmb3JlLFxuICAqOjphZnRlciB7XG4gICAgYm94LXNpemluZzogYm9yZGVyLWJveDtcbiAgfVxuXG4gIGJvZHkge1xuICAgIG1hcmdpbjogMDtcbiAgICBmb250LWZhbWlseTogc3lzdGVtLXVpO1xuICB9XG5gO1xuIl0sIm5hbWVzIjpbImNyZWF0ZUdsb2JhbFN0eWxlIiwiR2xvYmFsU3R5bGUiXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./styles.js\n");
+
+/***/ }),
+
+/***/ "./components/BoxWithClassName/BoxWithClassName.css":
+/*!**********************************************************!*\
+  !*** ./components/BoxWithClassName/BoxWithClassName.css ***!
+  \**********************************************************/
+/***/ (() => {
+
+
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("react/jsx-dev-runtime");
+
+/***/ }),
+
+/***/ "styled-components":
+/*!************************************!*\
+  !*** external "styled-components" ***!
+  \************************************/
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("styled-components");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./pages/_app.js"));
+module.exports = __webpack_exports__;
+
+})();
---
+++ react/nine-react-styled-components/react-styled-components_box/.next/server/pages/index.js
@@ -0,0 +1,76 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/index";
+exports.ids = ["pages/index"];
+exports.modules = {
+
+/***/ "./components/BoxWithClassName/BoxWithClassName.js":
+/*!*********************************************************!*\
+  !*** ./components/BoxWithClassName/BoxWithClassName.js ***!
+  \*********************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ BoxWithClassName)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n\nfunction BoxWithClassName({ isBlack  }) {\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"div\", {\n        className: `box-with-classname${isBlack ? \" box-with-classname--black\" : \"\"}`\n    }, void 0, false, {\n        fileName: \"/Users/macbookohnenamen/Desktop/spiced-bootcamp/challenges-spiced/react/nine-react-styled-components/react-styled-components_box/components/BoxWithClassName/BoxWithClassName.js\",\n        lineNumber: 3,\n        columnNumber: 5\n    }, this);\n}\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9jb21wb25lbnRzL0JveFdpdGhDbGFzc05hbWUvQm94V2l0aENsYXNzTmFtZS5qcy5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7QUFBQTtBQUFlLFNBQVNBLGlCQUFpQixFQUFFQyxRQUFPLEVBQUUsRUFBRTtJQUNwRCxxQkFDRSw4REFBQ0M7UUFDQ0MsV0FBVyxDQUFDLGtCQUFrQixFQUM1QkYsVUFBVSwrQkFBK0IsRUFBRSxDQUM1QyxDQUFDOzs7Ozs7QUFHUixDQUFDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcmVhY3Qtc3R5bGVkLWNvbXBvbmVudHNfYm94Ly4vY29tcG9uZW50cy9Cb3hXaXRoQ2xhc3NOYW1lL0JveFdpdGhDbGFzc05hbWUuanM/NWZmZCJdLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVmYXVsdCBmdW5jdGlvbiBCb3hXaXRoQ2xhc3NOYW1lKHsgaXNCbGFjayB9KSB7XG4gIHJldHVybiAoXG4gICAgPGRpdlxuICAgICAgY2xhc3NOYW1lPXtgYm94LXdpdGgtY2xhc3NuYW1lJHtcbiAgICAgICAgaXNCbGFjayA/IFwiIGJveC13aXRoLWNsYXNzbmFtZS0tYmxhY2tcIiA6IFwiXCJcbiAgICAgIH1gfVxuICAgID48L2Rpdj5cbiAgKTtcbn1cbiJdLCJuYW1lcyI6WyJCb3hXaXRoQ2xhc3NOYW1lIiwiaXNCbGFjayIsImRpdiIsImNsYXNzTmFtZSJdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./components/BoxWithClassName/BoxWithClassName.js\n");
+
+/***/ }),
+
+/***/ "./components/BoxWithStyledComponents/BoxWithStyledComponents.jsx":
+/*!************************************************************************!*\
+  !*** ./components/BoxWithStyledComponents/BoxWithStyledComponents.jsx ***!
+  \************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ isBlack)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! styled-components */ \"styled-components\");\n/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(styled_components__WEBPACK_IMPORTED_MODULE_1__);\n\n\nfunction isBlack() {\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"div\", {\n        children: [\n            /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"h1\", {\n                children: \"Box with Styled Components\"\n            }, void 0, false, {\n                fileName: \"/Users/macbookohnenamen/Desktop/spiced-bootcamp/challenges-spiced/react/nine-react-styled-components/react-styled-components_box/components/BoxWithStyledComponents/BoxWithStyledComponents.jsx\",\n                lineNumber: 6,\n                columnNumber: 7\n            }, this),\n            /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"p\", {\n                children: \"This is a box with Styled Components\"\n            }, void 0, false, {\n                fileName: \"/Users/macbookohnenamen/Desktop/spiced-bootcamp/challenges-spiced/react/nine-react-styled-components/react-styled-components_box/components/BoxWithStyledComponents/BoxWithStyledComponents.jsx\",\n                lineNumber: 7,\n                columnNumber: 7\n            }, this)\n        ]\n    }, void 0, true, {\n        fileName: \"/Users/macbookohnenamen/Desktop/spiced-bootcamp/challenges-spiced/react/nine-react-styled-components/react-styled-components_box/components/BoxWithStyledComponents/BoxWithStyledComponents.jsx\",\n        lineNumber: 5,\n        columnNumber: 5\n    }, this);\n}\nconst StyledBox = styled_components__WEBPACK_IMPORTED_MODULE_1___default().div.withConfig({\n    displayName: \"BoxWithStyledComponents__StyledBox\",\n    componentId: \"sc-44ae4a87-0\"\n})``;\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9jb21wb25lbnRzL0JveFdpdGhTdHlsZWRDb21wb25lbnRzL0JveFdpdGhTdHlsZWRDb21wb25lbnRzLmpzeC5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7OztBQUFBO0FBQXVDO0FBRXhCLFNBQVNDLFVBQVU7SUFDaEMscUJBQ0UsOERBQUNDOzswQkFDQyw4REFBQ0M7MEJBQUc7Ozs7OzswQkFDSiw4REFBQ0M7MEJBQUU7Ozs7Ozs7Ozs7OztBQUdULENBQUM7QUFFRCxNQUFNQyxZQUFZTCx1RUFBVTs7O0VBQUEsQ0FBQyxDQUFDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcmVhY3Qtc3R5bGVkLWNvbXBvbmVudHNfYm94Ly4vY29tcG9uZW50cy9Cb3hXaXRoU3R5bGVkQ29tcG9uZW50cy9Cb3hXaXRoU3R5bGVkQ29tcG9uZW50cy5qc3g/ZWZkNSJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgc3R5bGVkIGZyb20gXCJzdHlsZWQtY29tcG9uZW50c1wiO1xuXG5leHBvcnQgZGVmYXVsdCBmdW5jdGlvbiBpc0JsYWNrKCkge1xuICByZXR1cm4gKFxuICAgIDxkaXY+XG4gICAgICA8aDE+Qm94IHdpdGggU3R5bGVkIENvbXBvbmVudHM8L2gxPlxuICAgICAgPHA+VGhpcyBpcyBhIGJveCB3aXRoIFN0eWxlZCBDb21wb25lbnRzPC9wPlxuICAgIDwvZGl2PlxuICApO1xufVxuXG5jb25zdCBTdHlsZWRCb3ggPSBzdHlsZWQuZGl2YGA7XG4iXSwibmFtZXMiOlsic3R5bGVkIiwiaXNCbGFjayIsImRpdiIsImgxIiwicCIsIlN0eWxlZEJveCJdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./components/BoxWithStyledComponents/BoxWithStyledComponents.jsx\n");
+
+/***/ }),
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ HomePage)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _components_BoxWithClassName_BoxWithClassName_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../components/BoxWithClassName/BoxWithClassName.js */ \"./components/BoxWithClassName/BoxWithClassName.js\");\n/* harmony import */ var _components_BoxWithStyledComponents_BoxWithStyledComponents_jsx__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../components/BoxWithStyledComponents/BoxWithStyledComponents.jsx */ \"./components/BoxWithStyledComponents/BoxWithStyledComponents.jsx\");\n\n\n\nfunction HomePage() {\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"div\", {\n        children: [\n            /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(_components_BoxWithClassName_BoxWithClassName_js__WEBPACK_IMPORTED_MODULE_1__[\"default\"], {}, void 0, false, {\n                fileName: \"/Users/macbookohnenamen/Desktop/spiced-bootcamp/challenges-spiced/react/nine-react-styled-components/react-styled-components_box/pages/index.js\",\n                lineNumber: 6,\n                columnNumber: 7\n            }, this),\n            /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(_components_BoxWithClassName_BoxWithClassName_js__WEBPACK_IMPORTED_MODULE_1__[\"default\"], {\n                isBlack: true\n            }, void 0, false, {\n                fileName: \"/Users/macbookohnenamen/Desktop/spiced-bootcamp/challenges-spiced/react/nine-react-styled-components/react-styled-components_box/pages/index.js\",\n                lineNumber: 7,\n                columnNumber: 7\n            }, this)\n        ]\n    }, void 0, true, {\n        fileName: \"/Users/macbookohnenamen/Desktop/spiced-bootcamp/challenges-spiced/react/nine-react-styled-components/react-styled-components_box/pages/index.js\",\n        lineNumber: 5,\n        columnNumber: 5\n    }, this);\n}\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9pbmRleC5qcy5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7OztBQUFBO0FBQWtGO0FBQ3NCO0FBQ3pGLFNBQVNFLFdBQVc7SUFDakMscUJBQ0UsOERBQUNDOzswQkFDQyw4REFBQ0gsd0ZBQWdCQTs7Ozs7MEJBQ2pCLDhEQUFDQSx3RkFBZ0JBO2dCQUFDSSxPQUFPOzs7Ozs7Ozs7Ozs7QUFHL0IsQ0FBQyIsInNvdXJjZXMiOlsid2VicGFjazovL3JlYWN0LXN0eWxlZC1jb21wb25lbnRzX2JveC8uL3BhZ2VzL2luZGV4LmpzP2JlZTciXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IEJveFdpdGhDbGFzc05hbWUgZnJvbSBcIi4uL2NvbXBvbmVudHMvQm94V2l0aENsYXNzTmFtZS9Cb3hXaXRoQ2xhc3NOYW1lLmpzXCI7XG5pbXBvcnQgQm94V2l0aFN0eWxlZENvbXBvbmVudHMgZnJvbSBcIi4uL2NvbXBvbmVudHMvQm94V2l0aFN0eWxlZENvbXBvbmVudHMvQm94V2l0aFN0eWxlZENvbXBvbmVudHMuanN4XCI7XG5leHBvcnQgZGVmYXVsdCBmdW5jdGlvbiBIb21lUGFnZSgpIHtcbiAgcmV0dXJuIChcbiAgICA8ZGl2PlxuICAgICAgPEJveFdpdGhDbGFzc05hbWUgLz5cbiAgICAgIDxCb3hXaXRoQ2xhc3NOYW1lIGlzQmxhY2sgLz5cbiAgICA8L2Rpdj5cbiAgKTtcbn1cbiJdLCJuYW1lcyI6WyJCb3hXaXRoQ2xhc3NOYW1lIiwiQm94V2l0aFN0eWxlZENvbXBvbmVudHMiLCJIb21lUGFnZSIsImRpdiIsImlzQmxhY2siXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./pages/index.js\n");
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
+
+module.exports = require("react/jsx-dev-runtime");
+
+/***/ }),
+
+/***/ "styled-components":
+/*!************************************!*\
+  !*** external "styled-components" ***!
+  \************************************/
+/***/ ((module) => {
+
+module.exports = require("styled-components");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./pages/index.js"));
+module.exports = __webpack_exports__;
+
+})();
